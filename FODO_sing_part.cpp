cf6508f0702fe189b6943a63453426adaa7ad561
Stefano Pelli <pcsj@outlook.com>
2012-08-09 16:57:35 +0200

    loop per lettura da file esterno elementi + funzione al posto di while

--- FODO_sing_part.cpp
+++ FODO_sing_part.cpp
@@ -1,48 +1,5 @@
-#include <cstdio> 
-#include <cstdlib>
-#include <cmath>
-#include <iostream>
 
-#define EPSILON				1.0
-#define N_MAX_ELEMENTS		100	
-#define DRIFT_LENGTH		0.3
-#define CELL_LENGTH			0.2
-#define FODO_LENGTH			1.0
-#define ENERGIA				30.0
-#define CHARGE 1.602176565e-19
-#define MP_KG 1.6726231e-27
-#define MP_MEV 938.272013
-#define SPEED_OF_LIGHT 2.99792458e8
-//#define GRADIENTE_FOC1		9.616
-//#define GRADIENTE_DEF1		9.3786
-//#define GRADIENTE_FOC1		12.15
-//#define GRADIENTE_DEF1		11.85
-#define GRADIENTE_FOC1		10.0
-#define GRADIENTE_DEF1		10.0
-#define DRIFT				0
-#define FOC					1
-#define DEFOC				2
-#define n_step				100
-
-
-
-
-#define pos_x 0.0
-#define pos_y 0.0
-#define imp_x 0.05
-#define imp_y 0.05
-
-#ifdef __linux
-#include <fenv.h>
-#endif
-
-#define DEBUG
-#define MOD1
-//#define MOD2
-
-using namespace std;
-
-
+#include "FODO_sing_part.h"
 
 int dsMap(double L)
 {
@@ -53,7 +10,7 @@ int dsMap(double L)
 }
 
 
-double *prod(double * A,double ** N,double S)
+double *prod(double * A,vector< vector <double> > N,double S)
 {
 	double *H = new double[4];
 	for (int i=0; i<4; i++) H[i] = 0.0;
@@ -69,7 +26,7 @@ double *prod(double * A,double ** N,double S)
 
 
 
-double *optics(double ** N,int i)
+double *optics(vector< vector <double> > N,int i)
 {
 	double  oem,s,*A=new double[2];
 	oem=acos((N[i][i]+N[i+1][i+1])*0.5);
@@ -91,27 +48,21 @@ double *optics(double ** N,int i)
 	return A;
 }
 
-/*
-double **prod(double ** A, double ** B) 
+void prodo(vector< vector <double> > A, vector< vector <double> > B, int dimensione) 
 	{
-	double ** H = new double*[4];
-	for (int kk=0; kk < 4; kk++) H[kk] = new double[4];
-	for (int i=0; i<4; i++)
-		for (int j=0; j<4; j++) 
-			{
-	        	H[i][j]=0;
-	        	for (int k=0; k<4; k++)             
+	vector <vector <double> > H(4,vector<double>(4,0));
+
+	for (int i=0; i<dimensione; i++)
+		for (int j=0; j<dimensione; j++) 
+        	for (int k=0; k<dimensione; k++)             
 	        	        H[i][j] += A[i][k] * B[k][j];
-			for (int a=0;a<4;a++)
-				for (int i=0;i<4;i++)
+			for (int a=0;a<dimensione;a++)
+				for (int i=0;i<dimensione;i++)
 					A[a][i]=H[a][i];
-		        }
-	return A;
 	}
-*/
 
 		
-void  defocusing (double ** M, double d1, double S, int F, FILE * file)
+void  defocusing (vector< vector <double> > M, double d1, double S, FILE * file)
 		{
 			M[0][0]=M[1][1]=cosh(d1*S);
 			M[0][1]=sinh(d1*S)/d1;
@@ -127,7 +78,7 @@ void  defocusing (double ** M, double d1, double S, int F, FILE * file)
 			}
 		}
 
-void  focusing (double ** M, double d1, double S, int F, FILE * file)
+void  focusing (vector< vector <double> > M, double d1, double S, FILE * file)
 		{
 
 			M[0][0]=M[1][1]=cos(d1*S);
@@ -144,7 +95,7 @@ void  focusing (double ** M, double d1, double S, int F, FILE * file)
 			}
 		}
 
-void  drift (double ** M, double d1, double S, int F, FILE * file)
+void  drift (vector< vector <double> > M, double d1, double S, FILE * file)
 		{
 			M[0][0]=M[1][1]=M[2][2]=M[3][3]=1.;
 			M[0][1]=M[2][3]=S;
@@ -157,7 +108,7 @@ void  drift (double ** M, double d1, double S, int F, FILE * file)
 		}
 
 
-void scrivimatr (double ** M, FILE * output2)
+void scrivimatr2D (vector< vector <double> > M, FILE * output2)
 	{
 	for(int k=0; k < 4; k++)
 		{
@@ -165,6 +116,7 @@ void scrivimatr (double ** M, FILE * output2)
 		for(int j=0; j < 4; j++) fprintf(output2,"%+12.8f  ", M[k][j]);
 		}
 	}
+
 
 void scrividati (double s, double A[], double B[], double AMax, double AMin, double BMax, double BMin, FILE * file)
 	{
@@ -179,6 +131,86 @@ void scrividati (double s, double A[], double B[], double AMax, double AMin, dou
 		fprintf(file," %+10.5f",BMin);
 	}
 
+void inizializza3D(double ***M,int a, int dimensione)
+	{
+	M= new double**[a];
+	for (int kk=0; kk < dimensione; kk++)
+	{
+		M[kk] = new double*[dimensione];
+		for (int i=0; i < dimensione; i++) M[kk][i]= new double[dimensione];
+	}
+	
+	for (int k = 0; k < a; k++)
+		for (int i = 0; i < dimensione; i++)
+			for (int j = 0; j<dimensione; j++)
+				M[k][i][j]=0.0;
+	}
+
+void inizializza2D(double **M, int dimensione)
+	{
+	M= new double*[dimensione];
+	for (int kk=0; kk < dimensione; kk++)
+		M[kk] = new double[dimensione];
+	
+		for (int i = 0; i < dimensione; i++)
+			for (int j = 0; j<dimensione; j++)
+				M[i][j]=0.0;
+	}
+
+
+void similitudine_optics(vector< vector <double> > K, vector< vector <double> > OI, vector< vector <double> > O, vector< vector <double> > F, double * vett_i,double L,double S, double lunghezza_elemento, FILE *output2,FILE *posizionePart)
+{
+	while (S<lunghezza_elemento)
+	{
+	double * A=new double [2];
+	double * B=new double [2];
+	double AMax,AMin,BMax,BMin;
+
+	double dl=L/dsMap(L);
+
+		for (int i=0;i<4;i++)
+		{
+			for (int j=0;j<4;j++)
+			{
+				K[i][j]=0.0;
+				for (int a=0;a<4;a++)
+				{
+					for (int l=0;l<4;l++) K[i][j]+=O[i][a]*F[a][l]*OI[l][j];
+				}
+			}
+		}
+		for (int i=0;i<4;i++)
+		{
+			for (int j=0;j<4;j++) F[i][j]=K[i][j];
+		}
+		fprintf(output2,"\n Num_Step %f\n", S);
+		scrivimatr2D(F,output2);
+
+	A=optics (F,FOC);
+	B=optics (F,DEFOC);
+
+	vett_i=prod(vett_i,O,S);
+	fprintf(posizionePart," %+10.5f",S);
+	fprintf(posizionePart," %+10.5f",vett_i[0]);
+	fprintf(posizionePart," %+10.5f",vett_i[1]);
+	fprintf(posizionePart," %+10.5f",vett_i[2]);
+	fprintf(posizionePart," %+10.5f\n",vett_i[3]);
+
+		//Calcolo assi ellisse		
+
+		AMax= sqrt(A[1]*EPSILON);
+		AMin= sqrt((A[0]*A[0]+1)*EPSILON/A[1]);
+		BMax= sqrt(B[1]*EPSILON);
+		BMin= sqrt((B[0]*B[0]+1)*EPSILON/B[1]);
+	
+		//--------
+	
+		S+=dl;
+		scrividati(S,A,B,AMax,AMin,BMax,BMin,output2);
+	}
+}
+
+
 
 int main() 
 { 
@@ -186,66 +218,62 @@ int main()
 	feenableexcept(2);
 	feenableexcept(3);
 #endif
-	double ** F = new double*[4];
-	for (int kk=0; kk < 4; kk++) F[kk] = new double[4];
-	double ** Dx = new double*[4];
-	for (int kk=0; kk < 4; kk++) Dx[kk] = new double[4];
-	double ** Fx = new double*[4];
-	for (int kk=0; kk < 4; kk++) Fx[kk] = new double[4];
-	double ** I = new double*[4];
-	for (int kk=0; kk < 4; kk++) I[kk] = new double[4];
-	double ** K = new double*[4];
-	for (int kk=0; kk < 4; kk++) K[kk] = new double[4];
-	double ** O = new double*[4];
-	for (int kk=0; kk < 4; kk++) O[kk] = new double[4];
-	double ** OI = new double*[4];
-	for (int kk=0; kk < 4; kk++) OI[kk] = new double[4];
-	double ** FxI = new double*[4];
-	for (int kk=0; kk < 4; kk++) FxI[kk] = new double[4];
-	double ** DxI = new double*[4];
-	for (int kk=0; kk < 4; kk++) DxI[kk] = new double[4];
-	for (int a = 0; a < 4; a++)
-		for (int i = 0; i < 4; i++)
-			K[a][i]=I[a][i]=F[a][i]=O[a][i]=Fx[a][i]=Dx[a][i]=OI[a][i]=FxI[a][i]=DxI[a][i]=0.0;
-
 
 	FILE * output=fopen("risultatiFINALI.txt","w");
-//	FILE * output1=fopen("matrici.txt","w");
 	FILE * output2=fopen("Matrici_Prova.txt","w");
 	FILE * output3=fopen("Matrice.txt","w");
 	FILE * parametri=fopen("parametri.txt","r");
-		// qui di leggono tutti i dati	char *elemento=new char[N_MAX_ELEMENTS];	double * lunghezza= new double[N_MAX_ELEMENTS];	double * gradiente= new double[N_MAX_ELEMENTS];	int contatore=0;	while(1)  // loop infinito
+
+	// qui di leggono tutti i dati
+	char *elemento=new char[N_MAX_ELEMENTS];
+	double * lunghezza= new double[N_MAX_ELEMENTS];
+	double * gradiente= new double[N_MAX_ELEMENTS];
+	int contatore=0;
+	while(1)  // loop infinito
 		{
-		if(feof(parametri)) break;		if (contatore>=N_MAX_ELEMENTS) 			{			printf("\nTroppi Elementi\n");			break;			}		fscanf(parametri,"%c %lf %lf", &elemento[contatore], &lunghezza[contatore], &gradiente[contatore]);		contatore++;		}	
+		if(feof(parametri)) break;
+		if (contatore >= N_MAX_ELEMENTS) break;
+		fscanf(parametri,"%c %lf %lf", &elemento[contatore], &lunghezza[contatore], &gradiente[contatore]);
+		contatore++;
+		}	
 
-/****************
-	double EPSILON=1.0;
-	double DRIFT_LENGTH=0.3;
-	double CELL_LENGTH=0.2;
-	double FODO_LENGTH=1.0;
-	double ENERGIA=30.0;
-	double GRADIENTE_FOC1=12.15;
-	double GRADIENTE_DEF1=11.85;
-	int FOC=0;
-	int DEFOC=2;
-	int n_step=100;
-*****************/
+	//Qui inizializziamo le matrici, attenzione: Dx, O, Fx sono dei vettori di matrici e così anche OI,DxI,FxI
+
+	//double ** F = new double*[4];
+	//for (int kk=0; kk < 4; kk++) F[kk] = new double[4];
+	//double *** Dx, *** Fx, *** O, *** DxI, *** OI, *** FxI;
+	//double ** I, ** K; 
+	//inizializza3D(Dx,contatore,4);
+	//inizializza3D(Fx,contatore,4);
+	//inizializza3D(O,contatore,4);
+	//inizializza3D(DxI,contatore,4);
+	//inizializza3D(OI,contatore,4);
+	//inizializza3D(FxI,contatore,4);
+	//inizializza2D(K,4);
+	//inizializza2D(I,4);
 
 
-	//double f1 = sqrt(GRADIENTE_FOC1);
-	//double d1 = sqrt(GRADIENTE_DEF1);
+	vector <vector <double> > I(4,vector<double>(4,0));
+	vector <vector <double> > K(4,vector<double>(4,0));
+	vector <vector <double> > F(4,vector<double>(4,0));
+	vector <vector <vector <double> > > Fx(contatore,vector <vector <double> > (4, vector <double> (4,0.0)));
+	vector <vector <vector <double> > > Dx(contatore,vector <vector <double> > (4, vector <double> (4,0.0)));
+	vector <vector <vector <double> > > OI(contatore,vector <vector <double> > (4, vector <double> (4,0.0)));
+	vector <vector <vector <double> > > FxI(contatore,vector <vector <double> > (4, vector <double> (4,0.0)));
+	vector <vector <vector <double> > > DxI(contatore,vector <vector <double> > (4, vector <double> (4,0.0)));
+	vector <vector <vector <double> > > O(contatore,vector <vector <double> > (4, vector <double> (4,0.0)));
+
 	double gamma_beta=sqrt(2.0*ENERGIA/MP_MEV);
 	double gamma_v=gamma_beta*SPEED_OF_LIGHT;
 	double *f1 =new double [contatore];
 	double *d1 =new double [contatore];
 	for (int i=0;i<contatore;i++)
 		{
-		if (elemento[i]=="F")
+		if (elemento[i]=='F')
 		f1[i]=sqrt(gradiente[i]*CHARGE/(MP_KG*gamma_v));
-		if (elemento[i]=="D")
+		if (elemento[i]=='D')
 		d1[i]=sqrt(gradiente[i]*CHARGE/(MP_KG*gamma_v));
 		}
-//	double e = CELL_LENGTH;
 
 	double S=1.0;
 	S = S/n_step;
@@ -259,28 +287,43 @@ int main()
 
 	for (int i=0;i<contatore;i++)
 		{
-		if (elemento[i]=="F")
-		focusing(Fx,f1[i],CELL_LENGTH,FOC,output3);
-		else if (elemento[i]=="D")
-		defocusing(Dx,d1[i],CELL_LENGTH,DEFOC,output3);
-		else if (elemento[i]=="O")
-		drift(O,d1[i],DRIFT_LENGTH,DRIFT,output3);
+		if (elemento[i]=='F')
+		focusing(Fx[i],f1[i],CELL_LENGTH,output3);
+		else if (elemento[i]=='D')
+		defocusing(Dx[i],d1[i],CELL_LENGTH,output3);
+		else if (elemento[i]=='O')
+		drift(O[i],d1[i],DRIFT_LENGTH,output3);
 		else
 		printf("Qua si è sbagliato qualcosa");
 		}
 
 #ifdef DEBUG
 
-		fprintf(output2,"\nMATRICE DRIFT");
-		scrivimatr(O,output2);
-	
-		fprintf(output2,"\nMATRICE FOC.");
-		scrivimatr(Fx,output2);
+	for (int i=0;i<contatore;i++)
+	{
+		if (elemento[i]=='O')
+		{
+//			if (O[i][0][0] == 0.0) continue;
+			fprintf(output2,"\nMATRICE DRIFT");
+			scrivimatr2D(O[i],output2);
+			fprintf(output2,"\n");
+		}
+		else if (elemento[i]=='F')
+		{
+//			if (Fx[i][0][0] == 0.0) continue;
+			fprintf(output2,"\nMATRICE FOC.");
+			scrivimatr2D(Fx[i],output2);
+			fprintf(output2,"\n");
+		}
+		else if (elemento[i]=='D')
+		{
+//			if (Dx[i][0][0] == 0.0) continue;
+			fprintf(output2,"\nMATRICE DEFOC.");
+			scrivimatr2D(Dx[i],output2);
+			fprintf(output2,"\n");
+		}
 
-		fprintf(output2,"\nMATRICE DEFOC.");
-		scrivimatr(Dx,output2);
-
-		fprintf(output2,"\n");
+	}
 
 #endif	
 
@@ -289,40 +332,26 @@ int main()
 
 //	FODO composizione matrici
 
-
-	for (int a = 0; a < 4; a++)
-		for (int i = 0; i < 4; i++)
-			for (int j = 0; j < 4; j++)	
-				K[a][i] += Dx[a][j] * O[j][i];
-	for (int a = 0; a < 4; a++)
-		for (int i = 0; i < 4; i++)
-			for (int j = 0; j < 4; j++)	
-				I[a][i] += Fx[a][j] * O[j][i];
-	for (int a = 0; a < 4; a++)
-		for (int i = 0; i < 4; i++)
-			for (int j = 0; j < 4; j++)	
-				F[a][i] += I[a][j] * K[j][i];
-
-/*
-	fprintf(output1,"\nMATRICE FODO x");
-	for(int k=0; k < 4; k++)
+	vector <vector <double> > compos(4,vector<double>(4,0));
+	compos[0][0]=compos[1][1]=compos[2][2]=compos[3][3]=1.0;
+	
+	for (int i=0;i<contatore;i++)
+	{
+		if (elemento[i]=='O')
 		{
-			fprintf(output1,"\n");
-			for(int j=0; j < 4; j++) fprintf(output1,"%+12.8f  ", F[k][j]);
+			prodo(compos,O[i],4);
 		}
-	fprintf(output1,"\n");
-*/
-#ifdef DEBUG
-		fprintf(output2,"\nMATRICE DRIFT * FOC.");
-		scrivimatr(I,output2);
+		else if (elemento[i]=='F')
+		{
+			prodo(compos,Fx[i],4);
+		}
+		else if (elemento[i]=='D')
+		{
+			prodo(compos,Dx[i],4);
+		}
 
-		fprintf(output2,"\nMATRICE DRIFT * DEFOC.");
-		scrivimatr(K,output2);		
-
-		fprintf(output2,"\nMATRICE 1 * 2");
-		scrivimatr(F,output2);
-#endif
-
+	}
+	
 /************************************************************************/
 
 	// Calcolo Funzioni OTTICHE
@@ -353,100 +382,77 @@ int main()
 	N = (int) (2.*dsMap(DRIFT_LENGTH)+2.*dsMap(CELL_LENGTH));
 
 	S=DRIFT_LENGTH/dsMap(DRIFT_LENGTH);
-	O[0][0]=O[1][1]=O[2][2]=O[3][3]=1.;
-	O[0][1]=O[2][3]=S;
+//Calcolo MICROMAPPE per il Drift
+	for (int i=0; i < contatore; i++)
+	{
+		if (O[i][0][0] == 0.0) continue;
+		drift(O[i],0.0,S,output2);
+	}
+//e il suo inverso
+	for (int i=0; i < contatore; i++)
+	{
+		if (OI[i][0][0] == 0.0) continue;
+		drift(OI[i],0.0,S,output2);
+	}
+
+	S=CELL_LENGTH/dsMap(CELL_LENGTH);
+// per il Focusing
+	for (int i=0; i < contatore; i++)
+	{
+		if (Fx[i][0][0] == 0.0) continue;
+		focusing(Fx[i],f1[i],S,output2);
+	}
+//e il suo inverso
+	for (int i=0; i < contatore; i++)
+	{
+		if (FxI[i][0][0] == 0.0) continue;
+		focusing(FxI[i],f1[i],S,output2);
+	}
 	
-	#ifdef	MOD1
-		//inversi
-		OI[0][0]=OI[1][1]=OI[2][2]=OI[3][3]=1.0;
-		OI[0][1]=OI[2][3]=-S;
-	#endif	
+//per il Defocusing
+	for (int i=0; i < contatore; i++)
+	{
+		if (Dx[i][0][0] == 0.0) continue;
+		defocusing(Dx[i],d1[i],S,output2);
+	}
 
-	S=CELL_LENGTH/dsMap(CELL_LENGTH);	
-	Dx[0][0]=Dx[1][1]=cosh(d1*S);
-	Dx[0][1]=sinh(d1*S)/d1;
-	Dx[1][0]=sinh(d1*S)*d1;
-	Dx[2][2]=Dx[3][3]=cos(d1*S);
-	Dx[2][3]=sin(d1*S)/d1;
-	Dx[3][2]=-sin(d1*S)*d1;
-
-	Fx[0][0]=Fx[1][1]=cos(f1*S);
-	Fx[0][1]=sin(f1*S)/f1;
-	Fx[1][0]=-sin(f1*S)*f1;
-	Fx[2][2]=Fx[3][3]=cosh(f1*S);
-	Fx[2][3]=sinh(f1*S)/f1;
-	Fx[3][2]=sinh(f1*S)*f1;
-
-	#ifdef	MOD1
-		//inversi
-
-		DxI[0][0]=DxI[1][1]=cosh(-d1*S);
-		DxI[0][1]=sinh(-d1*S)/d1;
-		DxI[1][0]=sinh(-d1*S)*d1;
-		DxI[2][2]=DxI[3][3]=cos(-d1*S);
-		DxI[2][3]=sin(-d1*S)/d1;
-		DxI[3][2]=-sin(-d1*S)*d1;
-
-		FxI[0][0]=FxI[1][1]=cos(-f1*S);
-		FxI[0][1]=sin(-f1*S)/f1;
-		FxI[1][0]=-sin(-f1*S)*f1;
-		FxI[2][2]=FxI[3][3]=cosh(-f1*S);
-		FxI[2][3]=sinh(-f1*S)/f1;
-		FxI[3][2]=sinh(-f1*S)*f1;
-	#endif	
-
-	#ifdef	DEBUG
-
-		fprintf(output2,"\n\nNumero Esatto di Micromappe  %d\n\n",N);
-		
-		fprintf(output2,"\nMICROMATRICE DRIFT");
-		scrivimatr(O,output2);
-
-		fprintf(output2,"\nMICROMATRICE DEF.");
-		scrivimatr(Dx,output2);
-	
-		fprintf(output2,"\nMICROMATRICE FOC.");
-		scrivimatr(Fx,output2);
-
-	#endif
+//e il suo inverso
+		for (int i=0; i < contatore; i++)
+	{
+		if (DxI[i][0][0] == 0.0) continue;
+		drift(DxI[i],d1[i],S,output2);
+	}	
 
 	fprintf(output,"\n#   S  ");
 	fprintf(output,"      Alpha x  ");
 	fprintf(output,"   Beta x ");
 	fprintf(output,"  Alpha y  ");
 	fprintf(output,"  Beta y ");
-
-	//fprintf(output,"\n %+7.4f",S=0.0);
-	//fprintf(output," %+10.5f",A[1]);
-	//fprintf(output," %+10.5f",B[1]);
-	//fprintf(output," %+10.5f",A[0]);
-	//fprintf(output," %+10.5f",B[0]);
 	
 	AMax= sqrt(A[1]*EPSILON);
 	AMin= sqrt((A[0]*A[0]+1)*EPSILON/A[1]);
 	BMax= sqrt(B[1]*EPSILON);
 	BMin= sqrt((B[0]*B[0]+1)*EPSILON/B[1]);
 	
-		scrividati(0.0,A,B,AMax,AMin,BMax,BMin,output);
+	scrividati(0.0,A,B,AMax,AMin,BMax,BMin,output);
 
-// Primo ciclo per la O
+	for (int i=0;i<contatore;i++)
+	{
+		if (elemento[i]=='O')
+		{
+			similitudine_optics(K, OI[i], O[i], F, vett_i, DRIFT_LENGTH, S, DRIFT_LENGTH, output2, posizionePart);
+		}
+		else if (elemento[i]=='F')
+		{
+			similitudine_optics(K, FxI[i], Fx[i], F, vett_i, CELL_LENGTH, S, FODO_LENGTH, output2, posizionePart);
+		}
+		else if (elemento[i]=='D')
+		{
+			similitudine_optics(K, DxI[i], Dx[i], F, vett_i, CELL_LENGTH, S, CELL_LENGTH+DRIFT_LENGTH, output2, posizionePart);
+		}
 
-
-	similitudine_optics(K, OI, O, F, vett_i, DRIFT_LENGTH, S, DRIFT_LENGTH, output2, posizionePart);
-
-
-//Secondo Ciclo per la D in x e la F in y
-
-	similitudine_optics(K, DxI, Dx, F, vett_i, CELL_LENGTH, S, CELL_LENGTH+DRIFT_LENGTH, output2, posizionePart);
-
-//Terzo ciclo ancora per O
-
-	similitudine_optics(K, OI, O, F, vett_i, CELL_LENGTH, S, FODO_LENGTH-CELL_LENGTH, output2, posizionePart);	
-
-//Quarto Ciclo per la F in x e la D in y
-
-	similitudine_optics(K, FxI, Fx, F, vett_i, CELL_LENGTH, S, FODO_LENGTH, output2, posizionePart);
-
+	}
+		
 	fclose(output);
 	fclose(output2);
 	fclose(posizionePart);
@@ -454,57 +460,3 @@ int main()
 	fclose(output3);
 
 }
-
-
-void similitudine_optics(double ** K, double **OI, double **O, double **F, double * vett_i,double L,double S, double lunghezza_elemento, FILE *output2,FILE *posizionePart)
-	{
-	while (S<lunghezza_elemento)
-	{
-	double * A=new double A[2];
-	double * B=new double B[2];
-	double AMax,AMin,BMax,BMin;
-
-	double dl=L/dsMap(L);
-
-		for (int i=0;i<4;i++)
-		{
-			for (int j=0;j<4;j++)
-			{
-				K[i][j]=0.0;
-				for (int a=0;a<4;a++)
-				{
-					for (int l=0;l<4;l++) K[i][j]+=O[i][a]*F[a][l]*OI[l][j];
-				}
-			}
-		}
-		for (int i=0;i<4;i++)
-		{
-			for (int j=0;j<4;j++) F[i][j]=K[i][j];
-		}
-		fprintf(output2,"\n Num_Step %f\n", S);
-		scrivimatr(F,output2);
-
-	A=optics (F,FOC);
-	B=optics (F,DEFOC);
-
-	vett_i=prod(vett_i,O,S);
-	fprintf(posizionePart," %+10.5f",S);
-	fprintf(posizionePart," %+10.5f",vett_i[0]);
-	fprintf(posizionePart," %+10.5f",vett_i[1]);
-	fprintf(posizionePart," %+10.5f",vett_i[2]);
-	fprintf(posizionePart," %+10.5f\n",vett_i[3]);
-
-		//Calcolo assi ellisse		
-
-		AMax= sqrt(A[1]*EPSILON);
-		AMin= sqrt((A[0]*A[0]+1)*EPSILON/A[1]);
-		BMax= sqrt(B[1]*EPSILON);
-		BMin= sqrt((B[0]*B[0]+1)*EPSILON/B[1]);
-	
-		//--------
-	
-		S+=dl;
-		scrividati(S,A,B,AMax,AMin,BMax,BMin,output);
-	}
-
-	}
---
+++ FODO_sing_part.h
@@ -0,0 +1,75 @@
+#include <cstdio> 
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <iostream>
+
+#define EPSILON				1.0
+#define N_MAX_ELEMENTS		100	
+#define DRIFT_LENGTH		0.3
+#define CELL_LENGTH			0.2
+#define FODO_LENGTH			1.0
+#define ENERGIA				30.0
+#define CHARGE 1.602176565e-19
+#define MP_KG 1.6726231e-27
+#define MP_MEV 938.272013
+#define SPEED_OF_LIGHT 2.99792458e8
+//#define GRADIENTE_FOC1		9.616
+//#define GRADIENTE_DEF1		9.3786
+//#define GRADIENTE_FOC1		12.15
+//#define GRADIENTE_DEF1		11.85
+#define GRADIENTE_FOC1		10.0
+#define GRADIENTE_DEF1		10.0
+#define DRIFT				0
+#define FOC					1
+#define DEFOC				2
+#define n_step				100
+
+
+
+
+#define pos_x 0.0
+#define pos_y 0.0
+#define imp_x 0.05
+#define imp_y 0.05
+
+#ifdef __linux
+#include <fenv.h>
+#endif
+
+#define DEBUG
+#define MOD1
+//#define MOD2
+
+using namespace std;
+
+
+
+int dsMap(double );
+
+double *prod(double * ,vector< vector <double> >,double );
+
+double *optics(vector< vector <double> > ,int );
+
+void prodo(vector< vector <double> > , vector< vector <double> > , int );
+
+void  defocusing (vector< vector <double> > , double , double , FILE *);
+
+void  focusing (vector< vector <double> > , double , double , FILE * );
+
+void  drift (vector< vector <double> > , double , double , FILE * );
+
+void scrivimatr (vector< vector <double> > , FILE * );
+
+void scrividati (double , double [], double [], double , double , double , double , FILE * );
+
+void inizializza3D(double ***,int , int );
+
+void inizializza2D(double **, int );
+
+void similitudine_optics(vector< vector <double> >, vector< vector <double> >, vector< vector <double> >, vector< vector <double> >, double * ,double ,double , double , FILE *,FILE *);
+
+
+
+
+
